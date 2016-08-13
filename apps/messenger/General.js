6ae385a4b26c044a23a18c39fb4f418d0fadcb9d
keanine <keanen195@gmail.com>
2016-08-13 23:13:08 +0100

    Worked on Messenger Design

---
+++ apps/messenger/General.js
@@ -0,0 +1,19 @@
+﻿var sideBoxWidth = 300;
+
+function AddConversation()
+{
+    if (document.getElementById("tempConv"))
+    {
+        var AddConvButton = document.getElementById("tempConv");
+        AddConvButton.parentNode.removeChild(AddConvButton);
+    }
+
+    document.getElementById("conversations").innerHTML += '<div class="Conversation"><div class="ConvTitle">Unnamed Conversation</div>  <div class="ConvPreview">Conversation preview</div></div>';
+    document.getElementById("conversations").innerHTML += '<div id="tempConv" class="Conversation" onclick="AddConversation()"><div class="ConvTitle">Create New Conversation</div>  <div class="ConvPreview"></div></div>';
+
+}
+
+function ResizeMainHeight()
+{
+    //document.getElementById("SideBox").style.minHeight = "100%";
+}
