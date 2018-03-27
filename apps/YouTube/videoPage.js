8f6d56a9532351da02c95ac3763e39418c0cd179
Keanen <keanen195@gmail.com>
2018-03-27 18:29:37 +0100

    YT Quick Start

--- apps/YouTube/videoPage.js
+++ apps/YouTube/videoPage.js
@@ -1,10 +1,52 @@
 //AIzaSyAh8VJaUj7tQHZxiLw8zIRywHgAmqL1c1Y
 //var channelName = "error";
+/*document.addEventListener('DOMContentLoaded', function() { //alert("Ready!"); }, false);*/
 
-document.addEventListener('DOMContentLoaded', function() {
-    //alert("Ready!");
-}, false);
+(function()
+{
+    var OAUTH_CLIENT_ID = '132484806299-sjjkhh07be2h6s6l1h1uuvl7lrudh1s5.apps.googleusercontent.com';
+    var OAUTH_SCOPES = ['https://www.googleapis.com/auth/youtube',
+                        'https://www.googleapis.com/auth/youtube.force-ssl',
+                        'https://www.googleapis.com/auth/youtube.readonly',
+                        'https://www.googleapis.com/auth/youtube.upload',
+                        'https://www.googleapis.com/auth/youtubepartner',
+                        'https://www.googleapis.com/auth/youtubepartner-channel-audit'];
 
+    window.onJSClientLoad = function()
+    {
+        gapi.auth.init(function()
+        {
+            window.setTimeout(checkAuth, 1);
+        });
+    };
+
+    function checkAuth()
+    {
+        gapi.auth.authorize(
+        {
+            client_id : OAUTH_CLIENT_ID,
+            scope : OAUTH_SCOPES,
+            immediate : true
+
+        }, handleAuthResult);
+    }
+
+    function handleAuthResult(authResult)
+    {
+        if(authResult)
+        {
+            $('.pre-auth').hide();
+            $('post-auth').show();
+        }
+        else
+        {
+            $('.pre-auth').show();
+            $('post-auth').hide();
+            $('#login-link')
+        }
+    }
+
+})();
 
 function Start()
 {
