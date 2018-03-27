1a54b0fead00e275a22ce38671ef61b0d5fa76f7
Keanen <keanen195@gmail.com>
2018-03-27 17:29:13 +0100

    YT: Tutorial snippet

--- apps/YouTube/videoPage.js
+++ apps/YouTube/videoPage.js
@@ -3,43 +3,40 @@ var channelName = "error";
 
 document.addEventListener('DOMContentLoaded', function() {
     //alert("Ready!");
+    init();
     Start();
 }, false);
 
 
 function Start()
 {
-    // Loads the JavaScript client library and invokes `start` afterwards.
-    gapi.load('client', startAPI);
+    var request = gapi.client.youtube.search.list({
+        part: "snippet",
+        type: "video",
+        q: "vsauce",
+        maxResults: 5,
+        order: "viewCount"
+    });
 
+    request.execute(function(response)
+    {
+        document.getElementById("results").innerText = response;
+    });
 
     GetChannelName();
     document.getElementById("channelName").innerText = channelName;
 }
 
-function startAPI()
-{
-    // Initializes the client with the API key and the Translate API.
-    gapi.client.init({
-        'apiKey': 'AIzaSyAh8VJaUj7tQHZxiLw8zIRywHgAmqL1c1Y',
-        'discoveryDocs': ['https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest'],
-    }).then(function() {
-    // Executes an API request, and returns a Promise.
-    // The method name `language.translations.list` comes from the API discovery.
-    return gapi.client.language.translations.list({
-        q: 'hello world',
-        source: 'en',
-        target: 'de',
-        });
-    }).then(function(response) {
-        console.log(response.result.data.translations[0].translatedText);
-    }, function(reason) {
-        console.log('Errorre: ' + reason.result.error.message);
-        document.getElementById("results").innerText = 'Error: ' + reason.result.error.message;
-    });
-};
-
 function GetChannelName()
 {
     channelName = "Vsauce";
+}
+
+function init()
+{
+    gapi.client.setApiKey("AIzaSyAh8VJaUj7tQHZxiLw8zIRywHgAmqL1c1Y");
+    gapi.client.load("youtube", "v3", function()
+    {
+        //alert("YouTube API Ready!");
+    });
 }
