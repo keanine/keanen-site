ec61c505e83bda27ef91e76524d8c211e2a974c7
Keanen <keanen195@gmail.com>
2018-03-27 19:26:19 +0100

    YT Added clientID

--- apps/YouTube/videoPage.js
+++ apps/YouTube/videoPage.js
@@ -17,7 +17,7 @@
     // 'scope' field specifies space-delimited list of access scopes
 
     gapi.client.init({
-        'clientId': 'REPLACE_ME',
+        'clientId': '132484806299-sjjkhh07be2h6s6l1h1uuvl7lrudh1s5.apps.googleusercontent.com',
         'discoveryDocs': ['https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest'],
         'scope': 'https://www.googleapis.com/auth/youtube.force-ssl https://www.googleapis.com/auth/youtubepartner'
     }).then(function () {
